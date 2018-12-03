7525564778aa12912762ea505e25e9a652216313
Oliver Oxenham <oliver@cardcity.co>
2018-12-03 21:33:23 +0400

    - Now using React Suspense + Lazy successfully

--- src/index.js
+++ src/index.js
@@ -1,31 +1,31 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import * as serviceWorker from './serviceWorker';
-import asyncComponent from './appComponents/AsyncComponent/AsyncComponent';
 import 'normalize.css/normalize.css';
 
 import styles from './index.module.css';
 
+const Index = React.lazy(() => import('./Index/Index'));
+const App = React.lazy(() => import('./App/App'));
+const Editor = React.lazy(() => import('./Editor/Editor'));
 const rootEl = document.getElementById('root');
 
 const CodecraftorApp = () => {
-  const Index = asyncComponent(() =>
-    import('./Index/Index').then(module => module.default)
-  );
-  const App = asyncComponent(() =>
-    import('./App/App').then(module => module.default)
-  );
-  const Editor = asyncComponent(() =>
-    import('./Editor/Editor').then(module => module.default)
-  );
   return (
     <Router>
-      <div className={styles.app}>
-        <Route path="/" exact component={Index} />
-        <Route path="/craft/:craftId" component={App} />
-        <Route path="/editor" component={Editor} />
-      </div>
+      <React.Suspense fallback={<div>Loading...</div>}>
+        <div className={styles.app}>
+          <Switch>
+            <Route path="/" exact render={props => <Index {...props} />} />
+            <Route
+              path="/craft/:craftId"
+              render={props => <App {...props} />}
+            />
+            <Route path="/editor" render={props => <Editor {...props} />} />
+          </Switch>
+        </div>
+      </React.Suspense>
     </Router>
   );
 };
