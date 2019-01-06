62af32c0d4c933a3862496c518ae4c18a0fd8109
Oliver Oxenham <oliver@cardcity.co>
2019-01-06 21:59:48 +0400

    - fixed font usage - improved Text component

---
+++ src/templates/pioneer.js
@@ -0,0 +1,45 @@
+export default {
+  updated: 0,
+  ui: 'default',
+  template: 'pioneer',
+  theme: {
+    colors: {
+      background: '#FFF', // white
+      primary: '#FFE44B', // yellow
+      secondary: '#172a3b', // dark blue
+      tertiary: '#f9b82b', // dark yellow
+      quaternary: '#e4e6e8',
+      light: '#e4e6e8', // light gray
+      dark: '#172A3B'
+    },
+    font: {
+      family: ['Rajdhani', 'Open Sans'],
+      size: '14px'
+    }
+  },
+  globalCss: {},
+  pages: {
+    index: {
+      pageTitle: 'Index Page',
+      imports: [
+        'RootContainer',
+        'NavigationBar'
+      ],
+      root: {
+        componentType: 'RootContainer',
+        props: '{}',
+        editable: false,
+        childrenComponents: [
+          'zKx-xptnr'
+        ]
+      },
+      nonRootComponents: [
+        {
+          id: 'zKx-xptnr',
+          componentType: 'NavigationBar',
+          props: '{}'
+        }
+      ]
+    }
+  }
+};
