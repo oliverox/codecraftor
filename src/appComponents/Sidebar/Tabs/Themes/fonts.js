b5ef52e870f0fcc3c2d60fe72fb57658c7c40d7e
Oliver Oxenham <oliver@cardcity.co>
2019-01-16 14:28:55 +0400

    - Fixed multiple fonts - Added additional google fonts selection

--- src/appComponents/Sidebar/Tabs/Themes/fonts.js
+++ src/appComponents/Sidebar/Tabs/Themes/fonts.js
@@ -4,6 +4,38 @@ const fontList = [{
   value: 'Lora',
 }, {
   value: 'Montserrat',
+}, {
+  value: 'Rajdhani'
+}, {
+  value: 'Open Sans'
+}, {
+  value: 'Crimson Text'
+}, {
+  value: 'Work Sans'
+}, {
+  value: 'Oswald'
+}, {
+  value: 'Cardo'
+}, {
+  value: 'Quicksand'
+}, {
+  value: 'Archivo'
+}, {
+  value: 'Archivo Black'
+}, {
+  value: 'Judson'
+}, {
+  value: 'Abril Fatface'
+}, {
+  value: 'Roboto'
+}, {
+  value: 'Prata'
+}, {
+  value: 'Lato'
+}, {
+  value: 'Playfair Display'
+}, {
+  value: 'Chivo'
 }];
 
 const getFonts = () => {
